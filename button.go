2fd725faad0ab7993628b39bbb84a9bcb3e7bfc3
Ben Johnson <benbjohnson@yahoo.com>
2018-05-04 08:23:39 -0600

    Add Button.Target.

--- button.go
+++ button.go
@@ -16,6 +16,7 @@ type Button struct {
 	Href     string
 	Form     string
 	Value    string
+	Target   string
 	Size     string // "sm","lg"
 	Outline  bool
 	Block    bool
@@ -45,6 +46,7 @@ func (c *Button) Render(ctx context.Context, w io.Writer) {
 	c.renderClass(ctx, w)
 	c.renderHref(ctx, w)
 	c.renderForm(ctx, w)
+	c.renderTarget(ctx, w)
 	c.renderValue(ctx, w)
 	c.renderDisabled(ctx, w)
 
@@ -140,6 +142,14 @@ func (c *Button) renderForm(ctx context.Context, w io.Writer) {
 	if c.Form != "" {
 		io.WriteString(w, ` form="`)
 		io.WriteString(w, html.EscapeString(c.Form))
+		io.WriteString(w, `"`)
+	}
+}
+
+func (c *Button) renderTarget(ctx context.Context, w io.Writer) {
+	if c.Target != "" {
+		io.WriteString(w, ` target="`)
+		io.WriteString(w, html.EscapeString(c.Target))
 		io.WriteString(w, `"`)
 	}
 }
